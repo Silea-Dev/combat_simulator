e82228f21aa5cf61d0c3459dd567795408906e72
Silea-Dev <isaiaseloy8@gmail.com>
2025-09-12 10:25:19 -0300

    all edit

---
+++ main.py
@@ -0,0 +1,18 @@
+from core import game
+
+while True:
+    heroi = {}
+    nome_heroi_str = input("Digite o nome de seu heroi!\n")
+    confirmar_nome = input(
+        f"Deseja confirmar o nome de seu heroi: ({nome_heroi_str}) [S/N]?\n"
+    )
+    if confirmar_nome.lower() == "s":
+        print("iniciando batalha...")
+        heroi["nome"] = nome_heroi_str
+        heroi["vida"] = 100
+        heroi["ataque"] = 50
+        heroi["defesa"] = 20
+        game()
+        break
+    else:
+        continue

42a02331929e1b3fa3418a2526aa7d73c4b94732
Silea-Dev <isaiaseloy8@gmail.com>
2025-09-16 10:10:59 -0300

    feat: possibilidade de nova atualização

--- src/core.py
+++ src/core.py
@@ -130,3 +130,230 @@ def game():
             return print("Game over! É uma pena... mas tente novamente!")
         elif vilao["vida"] <= 0 and heroi["vida"] > 0:
             return print(f"PARABÉNS! Você derrotou {vilao['nome']}")
+
+
+"""import random as rd
+
+classes = ["espadachim", "mago", "curandeiro", "arqueiro"]
+
+# core
+print("Bem-vindo ao Campo de Batalha!!")
+# main
+vilao = {
+    "nome": "lucifer",
+    "classe": "demônio",
+    # "altura": rd.randint(5, 10),
+    "nivel": 100,
+    "vida": 2000,
+    "ataque": 200,
+    "defesa": 150,
+    "pet": "dragão",
+}
+
+VILAO_COPIADO = None
+HEROI_COPIADO = None
+
+
+def solicita_dados():
+    classe_str = input("Digite a classe de seu personagem: ")
+    return classe_str
+
+
+def espadachim(classe_str):
+    heroi = {
+        "classe": classe_str,
+        "nivel": 1,
+        "vida": 120,
+        "defesa": 15,
+        "ataque": 10,
+    }
+    return heroi
+
+
+def mago(classe_str):
+    heroi = {
+        "classe": classe_str,
+        "nivel": 1,
+        "vida": 100,
+        "defesa": 8,
+        "ataque": 20,
+    }
+    return heroi
+
+
+def arqueiro(classe_str):
+    heroi = {
+        "classe": classe_str,
+        "nivel": 1,
+        "vida": 100,
+        "defesa": 12,
+        "ataque": 15,
+    }
+    return heroi
+
+
+def curandeiro(classe_str):
+    heroi = {
+        "classe": classe_str,
+        "nivel": 1,
+        "vida": 100,
+        "defesa": 12,
+        "ataque": 15,
+    }
+    return heroi
+
+
+def classe_existente(classe_str):
+    if classe_str in classes:
+        return True
+    else:
+        return False
+
+
+def classe_escolhida(classe_str):
+    if classe_existente(classe_str):
+        if classe_str.lower() == "espadachim":
+            return espadachim(classe_str)
+        elif classe_str.lower() == "mago":
+            return mago(classe_str)
+        elif classe_str.lower() == "arqueiro":
+            return arqueiro(classe_str)
+        elif classe_str.lower() == "curandeiro":
+            return curandeiro(classe_str)
+    else:
+        return None
+
+
+def verificar_vida(heroi_hp, vilao_hp):
+    heroi_hp = HEROI_COPIADO["vida"]
+    vilao_hp = VILAO_COPIADO["vida"]
+    if heroi_hp > 0 and vilao_hp > 0:
+        return "continua"
+    elif vilao_hp <= 0 and heroi_hp > 0:
+        return True
+    elif heroi_hp <= 0 and vilao_hp > 0:
+        return False
+
+
+def condicao_vitoria(heroi_hp, vilao_hp):
+    if verificar_vida(heroi_hp, vilao_hp):
+        return True
+    else:
+        return False
+
+
+def acao(escolha):
+    if escolha == "0":
+        return "exit"
+    elif escolha.lower() == "1":
+        return "ataque"
+    elif escolha.lower() == "2":
+        return "defesa"
+    elif escolha.lower() == "3":
+        return "fugir"
+
+
+# core
+def game(naosei):
+    print("------Batalha iniciada!------\n")
+    while True:
+        if verificar_vida(HEROI_COPIADO, VILAO_COPIADO) == "continua":
+            escolha = input(
+                f"O {vilao['nome']} está na sua frente! o que deseja fazer?\nAtacar[1] | Defender[2] | Fugir[3] | end game [0]: "
+            )
+            if acao(escolha) == "exit":
+                print("Obrigado por ter jogado!")
+                return "exit"
+            elif acao(escolha) == "ataque":
+                chance_ataque = rd.randint(1, 4)
+                if chance_ataque == 1:
+                    VILAO_COPIADO["vida"] -= HEROI_COPIADO["ataque"] - VILAO_COPIADO    ["defesa"]
+                    print(f"{vilao['nome']} agora tem {vilao['vida']} de hp")
+                    continue
+                else:
+                    dano_verdadeiro = heroi["defesa"] - vilao["ataque"]
+                    if dano_verdadeiro < 0:
+                        novo_hp = heroi["vida"] + dano_verdadeiro
+                        heroi["vida"] = novo_hp
+                    else:
+                        print(
+                            f"O {vilao['nome']}o não teve dano para te atacar... nossa..."
+                        )
+
+                    print(
+                        f"------Errooou ataque! {vilao['nome']} lhe contra-atacou!\nNovo hp do heroi: {heroi['vida']}------\n"
+                    )
+            elif escolha.lower() == "2":
+                chance_defesa = rd.randint(1, 3)
+                if chance_defesa == 1:
+                    heroi["vida"] += 5
+                    print(
+                        f"------Defesa efetuada com sucesso! [ganho de mais 5 de hp!]\nNovo hp: {heroi["vida"]}------\n"
+                    )
+                    continue
+                else:
+                    heroi["vida"] *= 95 / 100
+                    print(
+                        f"------Defesa falhouuuuu! o Boss levou 5% hp!\nNovo hp: {heroi["vida"]}------\n"
+                    )
+                    continue
+
+            elif escolha.lower() == "3":
+                chance_fugir = rd.randint(1, 5)
+                if chance_fugir == 1:
+                    print("------Você fugiu com sucesso!------")
+                    break
+                else:
+                    print(
+                        "------Você não conseguiu fugir... o boss lhe acertou de raspão levando 10% hp!------\n"
+                    )
+                    heroi["vida"] *= 90 / 100
+                    continue
+        elif heroi["vida"] <= 0 and vilao["vida"] > 0:
+            return print("Game over! É uma pena... mas tente novamente!")
+        elif vilao["vida"] <= 0 and heroi["vida"] > 0:
+            return print(f"PARABÉNS! Você derrotou {vilao['nome']}")
+
+
+print("-----Jogo iniciado!-----")
+
+
+if __name__ == "__main__":
+    dados = solicita_dados()
+    heroi_escolhido = classe_escolhida(dados)
+    if heroi_escolhido is not None:
+        HEROI_COPIADO = heroi_escolhido.copy()
+        VILAO_COPIADO = vilao.copy
+    else:
+        print("escolha uma classe!")
+    verificar_vida(HEROI_COPIADO, VILAO_COPIADO)
+    naosei = verificar_vida(HEROI_COPIADO, VILAO_COPIADO)
+
+    while True:
+        saida = input("Deseja criar uma ficha de personagem[S/N]?\n")
+        if saida.lower() == "s":
+            nova_ficha = solicita_dados()
+            confirmar_ficha = input(
+                f"Deseja confirmar os dados:\n Nome: {nome_str}\n Classe: {classe_str}[S/N]?\n "
+            )
+            if confirmar_ficha.lower() == "n":
+                print("Refaça a lista [e garanta de preencher todos os dados!]")
+            elif confirmar_ficha.lower() == "s":
+                ficha_validada = validar_criar_ficha(
+                    nome_str,
+                    classe_str,
+                )
+                if ficha_validada is not None:
+                    return ficha_validada
+                else:
+                    print("[ERROR] níveis, dano, vida, são todos números inteiros!")
+            print(heroi)
+            game()
+            break
+        elif saida.lower() == "n":
+            print("Sem ficha, sem jogo. Volte quando quiser realmente jogar!")
+            break
+        else:
+            continue
+
+"""
